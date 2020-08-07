98f76062eb61294e47e3676d94b79165b0bb0525
takumab <takumab@gmail.com>
2020-08-07 22:48:43 +0100

    Add detail/show route, Add detail/show method to Groups controller

--- src/controllers/groupsController.ts
+++ src/controllers/groupsController.ts
@@ -3,7 +3,19 @@ const db = require("../db");
 
 class GroupsController {
   async index(req: Request, res: Response) {
-    const { rows } = await db.query("SELECT * FROM groups");
+    try {
+      const { rows } = await db.query("SELECT * FROM groups");
+      res.json(rows[0]);
+    } catch (e) {
+      res.json({ error: e.message });
+    }
+  }
+
+  async show(req: Request, res: Response) {
+    const { id } = req.params;
+    const { rows } = await db.query("SELECT groups.* FROM groups WHERE id=$1", [
+      id,
+    ]);
     res.json(rows[0]);
   }
 }
--- src/routes/groups.ts
+++ src/routes/groups.ts
@@ -5,5 +5,6 @@ const router = Router();
 const groupsController = new GroupsController();
 
 router.get("/", groupsController.index);
+router.get("/:id", groupsController.show);
 
 export default router;
